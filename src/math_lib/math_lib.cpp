2c18ad4f5fccb6fe37616d7719eaeb68b2614e43
DESKTOP-VN3VINC\Дмитро <kukuliak.dmytro.clg@chnu.edu.ua>
2024-09-20 09:31:18 +0300

    Feat: added LeastCommonMultiply function

--- src/math_lib/math_lib.cpp
+++ src/math_lib/math_lib.cpp
@@ -1,9 +1,23 @@
 #include "math_lib.h"
-
+#include <cmath>
 namespace MathLib
 {
-	bool isEqual(double a, double b, double tolerance)
-	{
-		return abs(a - b) <= tolerance;
-	}
+    bool isEqual(double a, double b, double tolerance)
+    {
+        return abs(a - b) <= tolerance;
+    }
+    int leastCommonMultiple(int a, int b)
+    {
+        int lcm = 1;
+        int maxNum = std::max(a, b);
+        for (int i = maxNum; i <= a * b; i += maxNum)
+        {
+            if (i % a == 0 && i % b == 0)
+            {
+                lcm = i;
+                break;
+            }
+        }
+        return lcm;
+    }
 }
