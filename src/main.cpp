76536f67e852cf0c6c4b66349b7ce0b6a62d4316
illiakhamula <i.hamula@chnu.edu.ua>
2024-10-05 09:20:12 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ using namespace MathLib;
 
 int main(int argc, char* argv[])
 {
-	double num1 = 10.5, num2 = 2.5;
+    double num1 = 10.5, num2 = 2.5;
 
     std::cout << "Addition: " << add(num1, num2) << std::endl;
     std::cout << "Subtraction: " << subtract(num1, num2) << std::endl;
