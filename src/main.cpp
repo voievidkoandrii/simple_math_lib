b362a8eef010a785f455a90086b52dae52c3c3e1
illiakhamula <i.hamula@chnu.edu.ua>
2024-10-11 00:06:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,13 @@ int main(int argc, char* argv[])
     std::cout << "Division: " << divide(num1, num2) << std::endl;
     std::cout << "GCD: " << GCD(num1, num2) << std::endl;
     std::cout << "Square root: " << sqrt(num1) << std::endl;
-    std::cout << "Least common multiple(4,6): " << leastCommonMultiple(4,6) << std::endl;
+
+    switch((int)leastCommonMultiple(4,6))
+    {
+        case 1:
+            std::cout << "The least common multiple is 1" << std::endl;
+        case 2:
+            std::cout << "The least common multiple is 2" << std::endl;
+    }
     return 0;
 }
